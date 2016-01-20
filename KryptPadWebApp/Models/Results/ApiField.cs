2c4eed1a241e93bfa380e7ed8c65ee826dcf39e8
Eric Butler <neptunecentury@hotmail.com>
2016-01-19 23:04:45 -0500

    Added ApiField

---
+++ KryptPadWebApp/Models/Results/ApiField.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KryptPadWebApp.Models.Results
+{
+    public class ApiField
+    {
+        /// <summary>
+        /// Gets or sets the name of the field. e.g Password
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Base64 encoded cipher text
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
---
+++ KryptPadWebApp/Models/Results/FieldsResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KryptPadWebApp.Models.Results
+{
+    public class FieldsResult
+    {
+    }
+}
