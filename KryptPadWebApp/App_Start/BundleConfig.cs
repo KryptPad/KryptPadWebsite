b4bab53bce0daf2ac589dc43d4138e1e3e8870f9
Eric <neptunecentury@hotmail.com>
2016-06-14 20:06:25 -0400

    Changes to app layout

--- KryptPadWebApp/App_Start/BundleConfig.cs
+++ KryptPadWebApp/App_Start/BundleConfig.cs
@@ -34,6 +34,9 @@ namespace KryptPadWebApp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css/signin").Include(
+                      "~/Content/SignIn.css"));
         }
     }
 }
