762d31cc4e92ffc3c80104639d3c85bee6874ae5
Eric Butler <neptunecentury@hotmail.com>
2018-09-09 13:12:12 -0400

    added favorites model and migration

---
+++ KryptPadWebApp/Models/Entities/Favorite.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KryptPadWebApp.Models.Entities
+{
+    public class Favorite
+    {
+
+        /// <summary>
+        /// Link back to item
+        /// </summary>
+        [JsonIgnore]
+        [Required]
+        public Item Item { get; set; }
+    }
+}
