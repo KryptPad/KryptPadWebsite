8e8794e7f0209aaef0fb66adcc2209468215d761
Eric Butler <ebutler@milestechnologies.com>
2018-09-10 15:53:20 -0400

    favorites

--- KryptPadWebApp/Controllers/FieldApiController.cs
+++ KryptPadWebApp/Controllers/FieldApiController.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Data.Entity;
 using KryptPadWebApp.Cryptography;
+using KryptPadWebApp.Models.Entities;
 
 namespace KryptPadWebApp.Controllers
 {
--- KryptPadWebApp/Models/Entities/FieldType.cs
+++ KryptPadWebApp/Models/Entities/FieldType.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace KryptPadWebApp.Models
+namespace KryptPadWebApp.Models.Entities
 {
     public class FieldType
     {
