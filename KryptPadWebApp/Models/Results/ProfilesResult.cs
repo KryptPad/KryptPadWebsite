2205333ae376ffd4ac3818aa3d5658eca3983481
Eric Butler <e.c.butler@outlook.com>
2022-09-09 01:08:34 -0400

    upgrade ef package

---
+++ KryptPadWebApp/Models/Results/ProfilesResult.cs
@@ -0,0 +1,23 @@
+﻿using KryptPadWebApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KryptPadWebApp.Models.Results
+{
+    public class ProfilesResult
+    {
+        public Profile[] Profiles { get; set; }
+
+        public ProfilesResult() { }
+        public ProfilesResult(Profile[] profiles)
+        {
+
+            Profiles = profiles;
+
+        }
+    }
+
+
+}
